b9e3b0f358846d428a2433f9c7e8f5fc41d7cd43
Mat Sz <contact@matsz.dev>
2020-04-10 12:59:07 +0200

    fixed example

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Letter } from '../.';
+import { Letter } from '../';
 
 const App = () => {
   return (
